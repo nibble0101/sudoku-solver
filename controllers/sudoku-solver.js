80a6b12c9c0df6b8a48340df6eb39e9234dcd5da
nibble0101 <mj.mawa@yahoo.com>
2021-10-20 18:36:42 +0300

    feat: add routes

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,23 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    return true;
   }
 
   solve(puzzleString) {
-    
+    return puzzleString;
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,23 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route("/api/solve").post((req, res) => {
+    const { puzzle } = req.body;
 
-    });
+    if (!puzzle.trim()) {
+      return res.json({ error: "Required field missing" });
+    }
+    if (!solver.validate(puzzle)) {
+      return res.json({ error: "Invalid characters in puzzle" });
+    }
+    if (puzzle.length !== 81) {
+      return res.json({ error: "Expected puzzle to be 81 characters long" });
+    }
+  });
 };
