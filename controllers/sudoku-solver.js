2bdbe774920cdfa0d5acfed35e732dc62fa8030c
nibble0101 <mj.mawa@yahoo.com>
2021-10-28 23:24:42 +0300

    fix: add more methods

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,3 +1,5 @@
+const rowLabels = "ABCDEFGHI";
+
 class SudokuSolver {
   validate(puzzleString) {
     if (/[^1-9\.]/.test(puzzleString)) {
@@ -10,100 +12,120 @@ class SudokuSolver {
     return "Valid puzzle string";
   }
 
-  generateLabels() {
-    const rowAndColLabels = [];
-    const rowLabels = "ABCDEFGHI";
-    const colLabels = "123456789";
-    for (const rowLabel of rowLabels) {
-      for (const colLabel of colLabels) {
-        rowAndColLabels.push(rowLabel + colLabel);
-      }
-    }
-    return rowAndColLabels;
-  }
+  // generateLabels() {
+  //   const rowAndColLabels = [];
+  //   const rowLabels = "ABCDEFGHI";
+  //   const colLabels = "123456789";
+  //   for (const rowLabel of rowLabels) {
+  //     for (const colLabel of colLabels) {
+  //       rowAndColLabels.push(rowLabel + colLabel);
+  //     }
+  //   }
+  //   return rowAndColLabels;
+  // }
 
-  generateGrid(puzzleString, rowAndColLabels) {
-    const grid = {};
-    for (let idx = 0; idx < rowAndColLabels.length; idx++) {
-      grid[rowAndColLabels[idx]] = puzzleString[idx];
+  // generateGrid(puzzleString, rowAndColLabels) {
+  //   const grid = {};
+  //   for (let idx = 0; idx < rowAndColLabels.length; idx++) {
+  //     grid[rowAndColLabels[idx]] = puzzleString[idx];
+  //   }
+  //   return grid;
+  // }
+
+  // generateUnits(rowAndColLabels) {
+  //   const units = {};
+
+  //   const board = [];
+  //   for (let idx = 0; idx < rowAndColLabels.length; idx += 9) {
+  //     const row = rowAndColLabels.slice(idx, idx + 9);
+  //     for (const square of row) {
+  //       units[square] = [row];
+  //     }
+  //     board.push(row);
+  //   }
+
+  //   const boxes = [];
+  //   for (let row = 0; row < board.length; row += 3) {
+  //     const boxRows = board.slice(row, row + 3);
+  //     const topmostRow = boxRows[0];
+  //     for (let col = 0; col < topmostRow.length; col += 3) {
+  //       const box = [];
+  //       for (const boxRow of boxRows) {
+  //         box.push(...boxRow.slice(col, col + 3));
+  //       }
+  //       boxes.push(box);
+  //     }
+  //   }
+  //   for (const box of boxes) {
+  //     for (const square of box) {
+  //       units[square].push(box);
+  //     }
+  //   }
+
+  //   const topmostRow = board[0];
+  //   for (let col = 0; col < topmostRow.length; col++) {
+  //     const column = [];
+  //     for (const row of board) {
+  //       column.push(row[col]);
+  //     }
+  //     for (const square of column) {
+  //       units[square].push(column);
+  //     }
+  //   }
+  //   return units;
+  // }
+
+  // generatePeers(units) {
+  //   const peers = {};
+  //   for (const square in units) {
+  //     for (const unit of units[square]) {
+  //       const uniquePeers = [...new Set([...unit[0], ...unit[1], ...unit[2]])];
+  //       for (let idx = 0; idx < uniquePeers.length; idx++) {
+  //         if (square === peerSquare[idx]) {
+  //           uniquePeers.splice(idx, 1);
+  //           peers[square] = uniquePeers;
+  //           break;
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return peers;
+  // }
+  // isValid(puzzleString, row, column, value) {
+  //   const grid = this.generateGrid(puzzleString);
+  //   const units = this.generateUnits(grid);
+  //   const peers = this.generatePeers(units);
+  //   for (const square of peers[`${row}${column}`]) {
+  //     if (grid[square] === `${value}`) return false;
+  //   }
+  //   return true;
+  // }
+
+  generateGrid(puzzleString) {
+    const grid = [];
+    for (let idx = 0; idx < puzzleString.length; idx += 9) {
+      const row = puzzleString.slice(idx, idx + 9);
+      grid.push(row);
     }
     return grid;
   }
 
-  generateUnits(rowAndColLabels) {
-    const units = {};
-
-    const board = [];
-    for (let idx = 0; idx < rowAndColLabels.length; idx += 9) {
-      const row = rowAndColLabels.slice(idx, idx + 9);
-      for (const square of row) {
-        units[square] = [row];
-      }
-      board.push(row);
-    }
-
-    const boxes = [];
-    for (let row = 0; row < board.length; row += 3) {
-      const boxRows = board.slice(row, row + 3);
-      const topmostRow = boxRows[0];
-      for (let col = 0; col < topmostRow.length; col += 3) {
-        const box = [];
-        for (const boxRow of boxRows) {
-          box.push(...boxRow.slice(col, col + 3));
-        }
-        boxes.push(box);
-      }
-    }
-    for (const box of boxes) {
-      for (const square of box) {
-        units[square].push(box);
-      }
-    }
-
-    const topmostRow = board[0];
-    for (let col = 0; col < topmostRow.length; col++) {
-      const column = [];
-      for (const row of board) {
-        column.push(row[col]);
-      }
-      for (const square of column) {
-        units[square].push(column);
-      }
-    }
-    return units;
-  }
-
-  generatePeers(units) {
-    const peers = {};
-    for (const square in units) {
-      for (const unit of units[square]) {
-        const uniquePeers = [...new Set([...unit[0], ...unit[1], ...unit[2]])];
-        for (let idx = 0; idx < uniquePeers.length; idx++) {
-          if (square === peerSquare[idx]) {
-            uniquePeers.splice(idx, 1);
-            peers[square] = uniquePeers;
-            break;
-          }
-        }
-      }
-    }
-    return peers;
-  }
-  isValid(puzzleString, row, column, value) {
-    const grid = this.generateGrid(puzzleString);
-    const units = this.generateUnits(grid);
-    const peers = this.generatePeers(units);
-    for (const square of peers[`${row}${column}`]) {
-      if (grid[square] === `${value}`) return false;
-    }
-    return true;
-  }
-
   checkRowPlacement(puzzleString, row, column, value) {
+    const grid = this.generateGrid(puzzleString);
+    const rowIndex = rowLabels.indexOf(row);
+    const row = grid[rowIndex];
+    for (let col = 0; col < row.length; col++) {
+      if (col !== column && row[col] === value) {
+        return false;
+      }
+    }
     return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    const grid = this.generateGrid(puzzleString);
+    const rowIndex = rowLabels.indexOf(row);
+
     return true;
   }
 
